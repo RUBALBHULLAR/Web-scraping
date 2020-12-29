b6720dd3783375f7c2dc1d72891c634f506560ac
RUBALBHULLAR <69098507+RUBALBHULLAR@users.noreply.github.com>
2020-12-29 03:06:34 -0700

    create index.html

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -5,3 +5,7 @@ from scrape_mars import scrape_all
 
 app = Flask(__name__)
 app.config ['MONGO_URI'] = 'mongodb://localhost:27017/mission_mars'
+mongo = PyMongo (app)
+
+db = mongo.db
+collection = db['mars_data']
